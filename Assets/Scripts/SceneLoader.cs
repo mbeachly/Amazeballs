9608ceda93133b758ae16c4bca305c5bbaed99d9
mbeachly <46512167+mbeachly@users.noreply.github.com>
2020-01-26 17:17:18 -0800

    Added Start Scene
    Added start scene with button to load the game scene via a Scene Loader object and script

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    // Based on Udemy 2D Unity course
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        // Go to next scene in build order
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+}
