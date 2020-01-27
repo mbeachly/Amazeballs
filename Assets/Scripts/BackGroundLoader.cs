60730795f480312078fc2b73117856d4b3578f59
mbeachly <46512167+mbeachly@users.noreply.github.com>
2020-01-26 20:01:19 -0800

    Load Background Image
    Loads background image from file specified in C# script

---
+++ Assets/Scripts/BackGroundLoader.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackGroundLoader : MonoBehaviour
+{
+    private void Start()
+    {
+        // Based on https://answers.unity.com/questions/541258/load-material-texture-from-file.html
+        string filePath = Application.dataPath + "/Materials/PunchbowlFallsU.png";
+
+        if (System.IO.File.Exists(filePath))
+        {
+            var bytes = System.IO.File.ReadAllBytes(filePath);
+            var tex = new Texture2D(1, 1);
+            tex.LoadImage(bytes);
+            GetComponent<Renderer>().material.mainTexture = tex;
+        }
+    }
+}
