fb86a186abaf80d89c1dd1ea88f9ff891eb21302
mbeachly <46512167+mbeachly@users.noreply.github.com>
2020-01-26 17:27:43 -0800

    You Won Screen
    Added a win screen that the game can go to when the ball reaches the end point.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -5,12 +5,22 @@ using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-    // Based on Udemy 2D Unity course
+    // Go to next scene in build order
+    // Based on Udemy course:
+    // Complete C# Unity Developer 2D: Learn to Code Making Games
     public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
 
-        // Go to next scene in build order
+
         SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    // Go to previous scene in build order
+    public void LoadPrevScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        SceneManager.LoadScene(currentSceneIndex - 1);
     }
 }
