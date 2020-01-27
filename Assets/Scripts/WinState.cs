e39ed45feb908b191ca9eafd89a2130d41385e10
Frank Smith <29299695+FrankCSmith@users.noreply.github.com>
2020-01-26 22:03:46 -0500

    Added game endpoint and load win screen

---
+++ Assets/Scripts/WinState.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WinState : MonoBehaviour
+{
+    SceneLoader scene;
+    // Load win screne when ball triggers red plane
+    private void OnTriggerEnter(Collider other)
+    {
+        GameObject gameObject = new GameObject("SceneLoader");
+        scene = gameObject.AddComponent<SceneLoader>();
+        scene.LoadNextScene();
+    }
+    
+}
