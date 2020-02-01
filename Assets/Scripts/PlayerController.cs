d44796f965a462986a0e104479487a4c62601941
mbeachly <46512167+mbeachly@users.noreply.github.com>
2020-01-31 21:17:05 -0800

    Acceleration Controls
    Added acceleration controls to make ball roll by tilting tablet, needs speed adjustment

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,11 +7,12 @@ public class PlayerController : MonoBehaviour
 {
     // public makes this accessible from Unity Inspector
     public float speed;
-
+    bool isFlat = true;
     private Rigidbody rb;
 
     void Start()
     {
+        
         rb = GetComponent<Rigidbody>();
         // Drop ball at start point
         rb.transform.position = new Vector3(Globals.startX, 1, Globals.startZ);
@@ -24,11 +25,21 @@ public class PlayerController : MonoBehaviour
     // Code from https://learn.unity.com/project/roll-a-ball-tutorial
     void FixedUpdate()
     {
-        float moveHorizontal = Input.GetAxis("Horizontal");
+        /* Keyboard controls
+        float moveHorizontal = Input.
         float moveVertical = Input.GetAxis("Vertical");
-
         Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
-
         rb.AddForce(movement * speed);
+        */
+
+        // Phone accelerometer controls
+        // https://www.youtube.com/watch?v=fsEkZLBeTJ8
+        Vector3 tilt = Input.acceleration;
+        if (isFlat) {
+            // Rotate so that up is perpendicular to phone surface
+            tilt = Quaternion.Euler(90, 0, 0) * tilt;
+        }
+
+        rb.AddForce(tilt);
     }
 }
