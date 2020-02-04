30766cf89acf22ee5bdc0db69af12ca6aa942bfe
mbeachly <46512167+mbeachly@users.noreply.github.com>
2020-02-03 17:21:20 -0800

    2D ball Start Scene
    Added glenn's 2D ball drop code to the Start game scene and added a space background

---
+++ Assets/Scripts/instantiateBall.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This class creates 2D balls that fall down
+// where users click on the start screen
+public class instantiateBall : MonoBehaviour
+{
+	public GameObject BallObj;
+	
+    // Start is called before the first frame update
+    public void Start()
+    {	
+
+	}
+
+    // Update is called once per frame
+ 
+	public void Update()
+	{
+		if (Input.GetButtonDown("Fire1"))
+        {
+				Vector2 worldPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Instantiate(BallObj, worldPosition, Quaternion.identity);
+        }
+	}
+}
