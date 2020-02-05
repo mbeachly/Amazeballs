e28af802890b4f7695c726ac518983db99e61bfa
mbeachly <46512167+mbeachly@users.noreply.github.com>
2020-02-04 20:58:18 -0800

    Main Menu
    Started Main Menu scene

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
-{
+{   
     // Go to next scene in build order
     // Based on Udemy course:
     // Complete C# Unity Developer 2D: Learn to Code Making Games
     public void LoadNextScene()
     {
         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-
-
         SceneManager.LoadScene(currentSceneIndex + 1);
     }
 
-    // Go to previous scene in build order
-    public void LoadPrevScene()
-    {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
 
-        SceneManager.LoadScene(currentSceneIndex - 1);
+    // Go to Main Menu
+    public void LoadMainMenu()
+    {   // Is there a better way to do this than hardcoding?
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    // Start the Game
+    public void LoadGame()
+    {   // Will the maze to load be controlled by global variable?
+        SceneManager.LoadScene("MazeGame");
     }
 
     // Exit the application
