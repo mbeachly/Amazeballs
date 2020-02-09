62a722c222ab44007e4bca24a7b3c38a8ae2b282
FrankCSmith <smithfc@gmail.com>
2020-02-09 12:40:28 -0500

    added SceneCapture scene and camera functionality

---
+++ Assets/Scripts/CaptureImage.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CaptureImage : MonoBehaviour
+{
+    WebCamTexture deviceCam;
+    public RawImage camDisplay;
+    SceneLoader scene;
+
+    public void StartCam()
+    {
+        if (deviceCam != null)
+        {
+            deviceCam.Stop();
+            deviceCam = null;
+            camDisplay.texture = null;
+            GameObject gameObject = new GameObject("SceneLoader");
+            scene = gameObject.AddComponent < SceneLoader>();
+            scene.LoadGame();
+        }
+        else { 
+            if (WebCamTexture.devices.Length > 0)
+            {
+                for (int i = 0; i < WebCamTexture.devices.Length; i++)
+                {
+                    if (!WebCamTexture.devices[i].isFrontFacing)
+                    {
+                        deviceCam = new WebCamTexture(WebCamTexture.devices[i].name, Screen.width, Screen.height);
+                    }
+                }
+                if (deviceCam != null)
+                {
+                    deviceCam.Play();
+                    camDisplay.texture = deviceCam;
+                }
+            }
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -11,6 +11,12 @@ public class SceneLoader : MonoBehaviour
         SceneManager.LoadScene("MainMenu");
     }
 
+    // Go to Capture Image
+    public void LoadCaptureImage()
+    {
+        SceneManager.LoadScene("CaptureImage");
+    }
+
     // Start the Game
     public void LoadGame()
     {   // Will the maze to load be controlled by global variable?
