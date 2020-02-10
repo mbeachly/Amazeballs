98dd0bb11e5c6d20214be20873e3d1ad85d848e3
FrankCSmith <smithfc@gmail.com>
2020-02-10 18:37:13 -0500

    added some comments

--- Assets/Scripts/CaptureImage.cs
+++ Assets/Scripts/CaptureImage.cs
@@ -10,8 +10,14 @@ public class CaptureImage : MonoBehaviour
     public Text startCaptureCamera;
     SceneLoader scene;
 
+    //Capture an image from device back facing camera and attach to maze plane
+    //Source:  https://docs.unity3d.com/ScriptReference/WebCamTexture.html
+    //Source:  https://www.youtube.com/watch?v=C_Q1QMomEyM
+    //Source:  https://stackoverflow.com/questions/24496438/can-i-take-a-photo-in-unity-using-the-devices-camera
+    
     public void StartCam()
     {
+        //If camera is already on when button clicked, get current pixels, convert to PNG and load image to Globals.tex
         if (deviceCam != null)
         {
             Texture2D image = new Texture2D(deviceCam.width, deviceCam.height);
@@ -21,8 +27,8 @@ public class CaptureImage : MonoBehaviour
             var tex = new Texture2D(1, 1);
             Globals.tex = tex;
             Globals.tex.LoadImage(bytes);
-            //GetComponent<Renderer>().material.mainTexture = tex;
         
+            //Stop live camera, set camera and raw image to null and load maze game
             deviceCam.Stop();
             deviceCam = null;
             camDisplay.texture = null;
@@ -30,7 +36,7 @@ public class CaptureImage : MonoBehaviour
             scene = gameObject.AddComponent <SceneLoader>();
             scene.LoadGame();
         }
-        else { 
+        else { //If a camera exists and is currently off, play back facing camera when start camera button clicked
             if (WebCamTexture.devices.Length > 0)
             {
                 for (int i = 0; i < WebCamTexture.devices.Length; i++)
@@ -44,14 +50,14 @@ public class CaptureImage : MonoBehaviour
                 {
                     deviceCam.Play();
                     camDisplay.texture = deviceCam;
-                    startCaptureCamera.text = "Capture Image";
+                    startCaptureCamera.text = "Capture Image";  //Change button text to capture image, on second click it will save image and start game
                 }
             }
         }
         
     }
 
-    // Update is called once per frame
+    // If Main Menu button is clicked, turn off camera if currently on and load main menu
     public void ExitToMainMenu()
     {
         if (deviceCam != null)
