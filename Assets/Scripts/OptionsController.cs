b437615768677170ba3b47702dcd79c9dfbb3a14
mbeachly <46512167+mbeachly@users.noreply.github.com>
2020-02-09 20:34:20 -0800

    Options fix
    Option settings are remembered now when leaving and returning to the Options scene

---
+++ Assets/Scripts/OptionsController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsController : MonoBehaviour
+{
+    Slider sizeSlider; 
+    Slider speedSlider;
+
+    PlayerController player;
+
+    // Start is called when Options scene is loaded
+    void Start()
+    {
+        player = GameObject.Find("Player Controller").GetComponent<PlayerController>();
+        // Load current settings
+        sizeSlider = GameObject.Find("SizeSlider").GetComponent<Slider>();
+        sizeSlider.value = player.GetBallSize(); 
+
+        speedSlider = GameObject.Find("SpeedSlider").GetComponent<Slider>();
+        speedSlider.value = player.GetBallSpeed();
+    }
+}
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,27 +6,48 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     // Ball Size
-    public static float ballSize = 1f; // Default = 1
+    private static float ballSize = 1f; // Default = 1
 
     // Ball Speed
-    public static float ballSpeed = 6f; // Default = 5
+    private static float ballSpeed = 6f; // Default = 5
 
-    // Start Point
+    // Start Point Coordinates
     public static int startX = 0;
     public static int startZ = 0;
 
     private Rigidbody rb;
 
-    // Change Ball Size
-    public void UpdateBallSize(float newSize)
+    /// <summary>
+    /// Sets Ball Size (radius of ball)
+    /// </summary>
+    /// <param name="newSize">floating point number (input from slider)</param>
+    public void SetBallSize(float newSize)
     {
         ballSize = newSize / 3;
     }
-
-    // Change Ball Speed
-    public void UpdateBallSpeed(float newSpeed)
+    /// <summary>
+    /// Gets Ball Size (radius of ball)
+    /// </summary>
+    /// <returns>integer (slider should be set to only whole numbers)</returns>
+    public int GetBallSize()
+    {   // Had issues with rounding without Ceil
+        return (int)Mathf.Ceil(3 * ballSize);
+    }
+    /// <summary>
+    /// Sets Ball Speed
+    /// </summary>
+    /// <param name="newSpeed">floating point number (input from slider)</param>
+    public void SetBallSpeed(float newSpeed)
     {
         ballSpeed = newSpeed * 2;
+    }
+    /// <summary>
+    /// Gets Ball Speed
+    /// </summary>
+    /// <returns>integer (slider should be set to only whole numbers)</returns>
+    public int GetBallSpeed()
+    {
+        return (int)ballSpeed / 2;
     }
 
     void Start()
@@ -34,6 +55,7 @@ public class PlayerController : MonoBehaviour
         rb = GetComponent<Rigidbody>();
         // Drop ball at start point
         rb.transform.position = new Vector3(startX, ballSize, startZ);
+        // Set ball to specified size
         rb.gameObject.transform.localScale = new Vector3(ballSize, ballSize, ballSize);
     }
 
